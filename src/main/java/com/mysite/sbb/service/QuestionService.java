c02679859a485b353939ec12d4f149df15fe768e
wjwj910 <https://github.com/wjwj910>
2024-12-18 15:39:54 +0900

    2-10

--- src/main/java/com/mysite/sbb/service/QuestionService.java
+++ src/main/java/com/mysite/sbb/service/QuestionService.java
@@ -1,11 +1,14 @@
 package com.mysite.sbb.service;
 
+import com.mysite.sbb.DataNotFoundException;
 import com.mysite.sbb.entity.Question;
 import com.mysite.sbb.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.zip.DataFormatException;
 
 @RequiredArgsConstructor
 @Service
@@ -15,5 +18,14 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
