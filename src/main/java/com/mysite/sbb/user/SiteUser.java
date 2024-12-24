7c01d41e34026ddd71564d0760acaebea7d90af2
wjwj910 <wjwj910@gmail.com>
2024-12-24 09:47:31 +0900

    3-07 and 3-06 refactoring

--- src/main/java/com/mysite/sbb/user/SiteUser.java
+++ src/main/java/com/mysite/sbb/user/SiteUser.java
@@ -1,6 +1,11 @@
 package com.mysite.sbb.user;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,5 +24,4 @@ public class SiteUser {
 
     @Column(unique = true)
     private String email;
-
 }
--- src/main/java/com/mysite/sbb/user/UserCreateForm.java
+++ src/main/java/com/mysite/sbb/user/UserCreateForm.java
@@ -3,13 +3,13 @@ package com.mysite.sbb.user;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UserCreateForm {
-
     @Size(min = 3, max = 25)
     @NotEmpty(message = "사용자ID는 필수항목입니다.")
     private String username;
